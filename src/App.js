caba03062be602669dee85d5e1089c4636c374ee
LaurynasKraujalis <laurynas_k@hotmail.com>
2020-08-19 14:00:10 +0200

    starterkit setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { getNASAPictures } from "./NasaAPI";
 
 function App() {
+  const [pictures, updatePictures] = React.useState(null);
+
+  useEffect(() => {
+    if (!pictures) {
+      const startDate = new Date("2020-07-01T08:28:41.917Z");
+      const endDate = new Date();
+      getNASAPictures(startDate, endDate).then((res) => {
+        updatePictures(res);
+      });
+    }
+  }, [pictures]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pictures &&
+        pictures.map((picture) => (
+          <div key={picture.date}>{picture.title}</div>
+        ))}
     </div>
   );
 }
